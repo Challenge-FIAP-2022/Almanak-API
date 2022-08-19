864e3f3207633b01160ff7f57068cdddb6beb275
OtavioMGomes <otavio.magalhaes.gomes@gmail.com>
2022-08-19 01:03:18 -0300

    + Initial Commit

---
+++ src/main/java/br/com/almanak/almanakApi/AlmanakApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.almanak.almanakApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmanakApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmanakApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/almanak/almanakApi/AlmanakApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.almanak.almanakApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmanakApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
