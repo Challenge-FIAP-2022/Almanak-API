ddc430c3c032bc2a9049e29e5a7748e8757ea9cc
OtavioMGomes <otavio.magalhaes.gomes@gmail.com>
2022-08-23 15:52:02 -0300

    + Ajuste de classes importadas

--- src/main/java/br/com/almanak/almanakApi/controller/AtividadeController.java
+++ src/main/java/br/com/almanak/almanakApi/controller/AtividadeController.java
@@ -2,15 +2,10 @@ package br.com.almanak.almanakApi.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
