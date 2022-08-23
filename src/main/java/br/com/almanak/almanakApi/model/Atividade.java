49f2aab6158561ae27f112748c453fc021c2c88e
OtavioMGomes <otavio.magalhaes.gomes@gmail.com>
2022-08-23 15:34:05 -0300

    +Ajuste nome tabela atividade

--- src/main/java/br/com/almanak/almanakApi/model/Atividade.java
+++ src/main/java/br/com/almanak/almanakApi/model/Atividade.java
@@ -13,13 +13,13 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tb_erro")
-@SequenceGenerator(name="erro", sequenceName="sq_erro", allocationSize=1)
+@Table(name="tb_atividade")
+@SequenceGenerator(name="atividade", sequenceName="sq_atividade", allocationSize=1)
 public class Atividade {
 
     @Id
-    @Column(name="id_erro")
-    @GeneratedValue(generator="erro", strategy = GenerationType.SEQUENCE)
+    @Column(name="id_atividade")
+    @GeneratedValue(generator="atividade", strategy = GenerationType.SEQUENCE)
     private Integer id;
 
     @Column(name="nm_tabela_alterada")
