9686bff1f10b3318182655fd982715cb06f361e3
OtavioMGomes <otavio.magalhaes.gomes@gmail.com>
2022-09-02 00:56:03 -0300

    + Ajuste tipo retorno chatbot

--- src/main/java/br/com/almanak/almanakApi/controller/ChatBotController.java
+++ src/main/java/br/com/almanak/almanakApi/controller/ChatBotController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ChatBotController {
 
     @GetMapping
-    public Object index(){
+    public Boolean index(){
         return true;
     }
     
