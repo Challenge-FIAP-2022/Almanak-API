5f86677b094a1c038cb57476a206d8cc6d3fdcff
OtavioMGomes <otavio.magalhaes.gomes@gmail.com>
2022-09-02 00:52:25 -0300

    + Chatbot Controller

---
+++ src/main/java/br/com/almanak/almanakApi/controller/ChatBotController.java
@@ -0,0 +1,16 @@
+package br.com.almanak.almanakApi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/chatbot")
+public class ChatBotController {
+
+    @GetMapping
+    public Object index(){
+        return true;
+    }
+    
+}
