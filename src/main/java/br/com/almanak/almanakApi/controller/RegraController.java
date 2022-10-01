e008485031b337251a6ebb18b8d70024d4a991e7
OtavioMGomes <otavio.magalhaes.gomes@gmail.com>
2022-10-01 15:51:51 -0300

    + Modulo de reposta de duvidas

---
+++ src/main/java/br/com/almanak/almanakApi/controller/RegraController.java
@@ -0,0 +1,48 @@
+package br.com.almanak.almanakApi.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.almanak.almanakApi.Interface.RegraDTO;
+import br.com.almanak.almanakApi.model.Regra;
+import br.com.almanak.almanakApi.service.RegraService;
+
+@RestController
+@RequestMapping("/api/regra")
+public class RegraController {
+
+    @Autowired
+    private RegraService service;
+
+    @GetMapping("resposta/{id}")
+    public ResponseEntity<List<RegraDTO>> listResponse(@PathVariable Integer id, @RequestBody @Valid String jsonString){
+        Optional<List<Regra>> opt = service.listResponse(id, jsonString);
+        
+        if(!opt.isEmpty()){
+            List<Regra> regras = opt.get();
+            List<RegraDTO> dtoList = new ArrayList<RegraDTO>();
+
+            for(Regra r : regras){
+                RegraDTO dto = new RegraDTO().convert(r);
+                dtoList.add(dto);
+            }
+
+            return ResponseEntity.ok().body(dtoList);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    
+}
--- src/main/java/br/com/almanak/almanakApi/repository/RegraRepository.java
+++ src/main/java/br/com/almanak/almanakApi/repository/RegraRepository.java
@@ -1,11 +1,18 @@
 package br.com.almanak.almanakApi.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.almanak.almanakApi.model.Regra;
 
 @Repository
 public interface RegraRepository extends JpaRepository<Regra, Integer> {
+
+    @Query(value="select * from fn_regra_resposta(?1, ?2)", nativeQuery = true) 
+    Optional<List<Regra>> listResponse(Integer id, String jsonString);
     
 }
